fb7d5b00846f3ba91bf2aad5fbeb0ad291b6407b
Douglas Modena <d.modena@live.com>
2023-10-16 09:03:52 -0300

    Introduce Services layer

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,5 +1,4 @@
-using BlacksmithAPI.Models;
-using BlacksmithAPI.Repositories;using Microsoft.AspNetCore.Mvc;
+using BlacksmithAPI.Models;using BlacksmithAPI.Services;using Microsoft.AspNetCore.Mvc;
 
 namespace BlacksmithAPI.Controllers;
 
@@ -10,63 +9,7 @@ public class OrderController : ControllerBase
     [HttpPost(Name = "Request Order")]
     public Order RequestOrder([FromBody] OrderRequest request)
     {
-        // Check client
-        CustomerRepository customerRepository = new CustomerRepository();
-        Customer customer = customerRepository.GetById(request.CustomerId);
-
-        // Check inventory
-        InventoryRepository inventoryRepository = new InventoryRepository();
-        float materialModifier = inventoryRepository.GetMaterialPriceModifier(request.Material);
-        
-        // Calculate price
-        int price = 0;
-        if (request.ItemType == "Sword")
-        {
-            price = 5 * (int)materialModifier;
-        }
-        else if (request.ItemType == "Spear")
-        {
-            price = 15 * (int)materialModifier;
-        }
-        else if (request.ItemType == "Shield")
-        {
-            price = 25 * (int)materialModifier;
-        }
-
-        // Calculate delivery date
-        DateTime deliveryDate = DateTime.Now.AddDays(2);
-        if (request.ItemType == "Sword")
-        {
-            deliveryDate = deliveryDate.AddDays(1);
-        }
-        else if (request.ItemType == "Spear")
-        {
-            deliveryDate = deliveryDate.AddDays(2);
-        }
-        else if (request.ItemType == "Shield")
-        {
-            deliveryDate = deliveryDate.AddDays(3);
-        }
-        
-        // Express customers have quicker delivery
-        if (customer.Category == "Express")
-        {
-            deliveryDate = deliveryDate.AddDays(-2);
-        }
-
-
-        // Premium customers have quicker delivery and an 10% discount
-        if(customer.Category == "Premium")
-        {
-            deliveryDate = deliveryDate.AddDays(-2);
-            price = (int)(price * 0.9);
-        }
-
-        return new Order()
-        {
-            Id = 1,
-            Price = price,
-            DeliveryDate = deliveryDate
-        };
+        var service = new OrderService();
+        return service.RequestOrder(request);
     }
 }
